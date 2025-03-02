4e68072265d97325d32542c86d49f55da25329fb
RobomrFox <avikp2002@gmail.com>
2025-03-01 20:19:27 -0700

    Tiptap

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import Tiptap from './components/tiptap'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <div className="card">
+        <Tiptap />
+      </div>
     </>
   )
 }
